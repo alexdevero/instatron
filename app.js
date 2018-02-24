3d88bae873f77f53d9ed1b5219f2be0a31b7eb3f
Alex Devero <deveroalex@gmail.com>
2018-02-24 15:04:24 +0100

    Implement menu, add cleaning options

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow, session} = require('electron') // http://electron.atom.io/docs/api
+const {app, BrowserWindow, dialog, Menu, session} = require('electron') // http://electron.atom.io/docs/api
 
 let window = null
 
@@ -25,6 +25,8 @@ app.once('ready', () => {
     height: 667,
     // Don't show the window until it ready, this prevents any white flickering
     show: false,
+    title: 'Instatron',
+    toolbar: true,
     webPreferences: {
       // Disable node integration in remote page
       nodeIntegration: false
@@ -36,10 +38,92 @@ app.once('ready', () => {
 
   // Query all cookies associated with a specific url.
   session.defaultSession.cookies.get({}, (error, cookies) => {
-    console.log(error, cookies)
+    // console.log(error, cookies)
   })
 
+
+  // Function for clearing cache
+  const win = BrowserWindow.getAllWindows()[0]
+  const ses = win.webContents.session
+  const clearAppCache = () => {
+    ses.clearCache(() => {
+      dialog.showMessageBox({type: 'info', buttons: ['OK'], message: 'Cache cleared.'})
+    })
+  }
+
+  // Template for menu
+  const menuTemplate = [
+    {
+      label: 'Edit',
+      submenu: [
+        {role: 'undo'},
+        {role: 'redo'},
+        {role: 'cut'},
+        {role: 'copy'},
+        {role: 'paste'},
+        {role: 'delete'}
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {role: 'reload'},
+        {role: 'forcereload'},
+        {role: 'resetzoom'},
+        {role: 'zoomin'},
+        {role: 'zoomout'}
+      ]
+    },
+    {
+      role: 'window',
+      submenu: [
+        {role: 'minimize'},
+        {role: 'close'}
+      ]
+    },
+    {
+      label: 'Maintenance',
+      submenu: [
+        {
+          label: 'Clear history',
+          click: () => {window.webContents.clearHistory(dialog.showMessageBox({type: 'info', buttons: ['OK'], message: 'History cleaned.'}))}
+        },
+        {
+          label: 'Clear cache',
+          click: () => {clearAppCache()}
+        },
+        {
+          label: 'Clear storage data',
+          click: () => {window.webContents.session.clearStorageData(dialog.showMessageBox({ type: 'info', buttons: ['OK'], message: 'Storage data cleaned.'}))}
+        },
+        {
+          label: 'Check cache size',
+          click: () => {window.webContents.session.getCacheSize((size) => dialog.showMessageBox({type: 'info', buttons: ['OK'], message: `Cache size is: ${size} bytes.`}))}
+        }
+      ]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click: () => {require('electron').shell.openExternal('https://github.com/alexdevero/instatron')}
+        },
+        {
+          label: 'Author',
+          click: () => {require('electron').shell.openExternal('https://alexdevero.com')}
+        }
+      ]
+    }
+  ]
+
+  // Build menu from menuTemplate
+  const menu = Menu.buildFromTemplate(menuTemplate)
+
   window.loadURL(url, {userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B137 Safari/601.1'})
+
+  // Set menu to menuTemplate
+  Menu.setApplicationMenu(menu)
 
   // Show window when page is ready
   window.once('ready-to-show', () => {
