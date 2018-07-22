3fcb6c78fed1bd4ab2ec9499a13c4826a148ecd7
Alex Devero <deveroalex@gmail.com>
2018-07-22 10:14:45 +0200

    Clear cache before closing the app

--- app.js
+++ app.js
@@ -143,10 +143,16 @@ app.once('ready', () => {
   })
 })
 
+app.on('before-quit', () => {
+  // Clear cache
+  clearAppCache()
+})
+
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
+
   if (process.platform !== 'darwin') {
     app.quit()
   }
