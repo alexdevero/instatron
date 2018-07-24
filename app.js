abe642dd12f90e63433ec0e195cf1f102487d6e2
Alex Devero <deveroalex@gmail.com>
2018-07-24 09:54:08 +0200

    Add ability to minimize app to system tray

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-const {app, BrowserWindow, dialog, Menu, session} = require('electron') // http://electron.atom.io/docs/api
+const { app, BrowserWindow, dialog, Menu, platform, session, Tray } = require('electron') // http://electron.atom.io/docs/api
+
+const path = require('path')
 
 require('electron-context-menu')({
   prepend: (params, browserWindow) => []
 })
 
 let window = null
+let appIcon = null
+let trayIcon = null
 
 // Temporary fix broken high-dpi scale factor on Windows (125% scaling)
 // info: https://github.com/electron/electron/issues/9691
 if (process.platform === 'win32') {
   app.commandLine.appendSwitch('high-dpi-support', 'true')
   app.commandLine.appendSwitch('force-device-scale-factor', '1')
+}
+
+if (platform == 'darwin') {
+  trayIcon = path.join(__dirname, 'assets/instagram-uploader-icon.png')
+} else if (platform == 'win32') {
+  trayIcon = path.join(__dirname, 'assets/instagram-uploader-icon.ico')
+} else {
+  trayIcon = path.join(__dirname, 'assets/instagram-uploader-icon.ico')
 }
 
 // session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
@@ -29,6 +41,7 @@ app.once('ready', () => {
     height: 667,
     // Don't show the window until it ready, this prevents any white flickering
     show: false,
+    icon: trayIcon,
     title: 'Instatron',
     toolbar: true,
     webPreferences: {
@@ -128,12 +141,67 @@ app.once('ready', () => {
   // Build menu from menuTemplate
   const menu = Menu.buildFromTemplate(menuTemplate)
 
+  // Set menu to menuTemplate
+  Menu.setApplicationMenu(menu)
+
+  // Create tray icon
+  appIcon = new Tray(trayIcon)
+
+  // Create RightClick context menu for tray icon
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Restore app',
+      click: () => {
+        window.show()
+      }
+    },
+    {
+      label: 'Close app',
+      click: () => {
+        window.close()
+      }
+    }
+  ])
+
+  // Set title for tray icon
+  appIcon.setTitle('Khan Academy')
+
+  // Set toot tip for tray icon
+  appIcon.setToolTip('Khan Academy')
+
+  // Create RightClick context menu
+  appIcon.setContextMenu(contextMenu)
+
+  // Always highlight the tray icon
+  appIcon.setHighlightMode('always')
+
+  // The tray icon is not destroyed
+  appIcon.isDestroyed(false)
+
+  // Restore (open) app after clicking on tray icon
+  // if window is already open, minimize it to system tray
+  appIcon.on('click', () => {
+    window.isVisible() ? window.hide() : window.show()
+  })
+
   window.loadURL(url, {
     userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B137 Safari/601.1'
   })
 
-  // Set menu to menuTemplate
-  Menu.setApplicationMenu(menu)
+  // Emitted when the window is closed.
+  window.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    window = null
+  })
+
+  // Minimize window to system tray
+  window.on('minimize',function(event){
+    event.preventDefault()
+    // window.minimize()
+    window.hide()
+})
 
   // Show window when page is ready
   window.once('ready-to-show', () => {
